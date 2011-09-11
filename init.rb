743d43eec937f93ab0489e735c2df356b53590d4
Paolo Freuli <paolo.freuli@gmail.com>
2011-09-12 00:07:54 +0200

    third commit

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_logindata do
   name 'Redmine Logindata plugin'
   author 'Wuatanabe'
   description 'Wrap Login and similar account related actions, in order to log more info.'
-  version '0.0.2'
+  version '0.1.0'
   url 'https://github.com/wuatanabe/RedmineLoginData'
   author_url 'https://github.com/wuatanabe'
 end
