b19fff2b0e170104d4163a9713b1aa0ce6e89923
Paolo Freuli <paolo.freuli@gmail.com>
2011-09-23 22:54:28 +0200

    v0.2

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ require 'log_login_patch'
 Redmine::Plugin.register :redmine_logindata do
   name 'Redmine Logindata plugin'
   author 'Paolo Freuli'
-  description 'On Login and related actions, log both ip and username'
+  description 'On Login and related actions, logs both IP and USERNAME'
   version '0.2.0'
   url 'https://github.com/wuatanabe/RedmineLoginData'
   author_url 'https://github.com/wuatanabe'
