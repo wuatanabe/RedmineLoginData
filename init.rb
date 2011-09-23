ccaab2db877a761eca3ff0ce5c175ca685acf3de
Paolo Freuli <paolo.freuli@gmail.com>
2011-09-23 22:52:51 +0200

    v0.2

--- init.rb
+++ init.rb
@@ -1,12 +1,16 @@
 require 'redmine'
+
 require 'log_login_patch'
 
 Redmine::Plugin.register :redmine_logindata do
   name 'Redmine Logindata plugin'
-  author 'Wuatanabe'
-  description 'Wrap Login and similar account related actions, in order to log more info.'
-  version '0.1.0'
+  author 'Paolo Freuli'
+  description 'On Login and related actions, log both ip and username'
+  version '0.2.0'
   url 'https://github.com/wuatanabe/RedmineLoginData'
   author_url 'https://github.com/wuatanabe'
 end
 
+puts "*"*30
+puts "Loading Logindata"
+puts "*"*30
--- lib/log_login_patch.rb
+++ lib/log_login_patch.rb
@@ -5,22 +5,59 @@ module AccountControllerPatch
     base.send(:include, InstanceMethods)
 
     base.class_eval do
-      alias_method_chain :login, :log 
+      alias_method_chain :login, :msg1 
+      alias_method_chain :successful_authentication, :msg2
+      alias_method_chain :logout, :msg3 
+      
+      # Note: not managing login failure (already correctly managed in account_controller.inve
+      # alias_method_chain :invalidcredential, :msg4
     end
   end
   
   module InstanceMethods
-    # Add some additional log to login 
-  def login_with_log
-	if request.get?	  
-	## puts " ****  Loading login form: Request IP is "+request.ip.to_s
-	else
-	 logger.warn   "**01** Login attempt with user #{params[:username]} from "+ request.remote_ip.to_s +" at #{Time.now.utc}"
-end
-	# Wrapped original login method
-	login_without_log	  
-  end
     
+  # Adds a rates tab to the user administration page
+  def login_with_msg1
+	if request.get?	  
+		## When just loading the login form
+		## puts "** GET **  Request IP is "+request.ip.to_s
+	else
+		## When submittiing login and password	
+		logger.info "Login attempt with user '#{params[:username]}' from IP #{request.remote_ip} at #{Time.now.utc}"
+	end
+	login_without_msg1	  
+		
+	### this is login content method (called by login_without_log wrapped by login_with_log)
+	#~ if request.get?    
+	#~ logout_user
+	#~
+	#~ end
+  end
+  
+  
+def logout_with_msg3
+begin
+     logger.info "Logout received from user '#{User.current.login}' from IP #{request.remote_ip} at #{Time.now.utc}"
+rescue => error
+     logger.info "ERROR: User.current.login exception"
+     logger.info "Logout received from IP #{request.remote_ip} at #{Time.now.utc}"
+end
+     logout_without_msg3  
+end
+    
+def successful_authentication_with_msg2(user)
+     logger.info "Login successful for user '#{params[:username]}' from IP #{request.remote_ip} at #{Time.now.utc}"
+     successful_authentication_without_msg2(user)	  
+end	
+
+
+#### -Note: not managing login failure (already correctly managed in account_controller.inve
+#~ def invalid_credentials
+#~ puts "** Message.04 ** Invalid credential for user #{params[:username]} from IP  "+ request.ip.to_s + " at #{Time.now.to_s}" 
+#~ invalidcredential_without_msg4  
+#~ end	
+
+
   end
 end
 
